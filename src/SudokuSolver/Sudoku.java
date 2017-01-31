033d92d0260ab43e0e0f95a8c118de691d261aaa
Dr Bob <Dr Bob@DrBob-PC>
2017-01-31 02:02:15 -0800

    Organizing main Sudoku initializing

--- src/SudokuSolver/Sudoku.java
+++ src/SudokuSolver/Sudoku.java
@@ -1,29 +1,37 @@
 /**
  * Builder and organization aide for a Sudoku puzzle.
+ * Also solves them, which is what this is built for!
  * @author drbob132
- * 
+ * @version 0.1
+ * @date 1/21/2017
  */
 
 package SudokuSolver;
 
+
 public class Sudoku {
 	public static final int SUDOKU_BLOCK_LENGTH = 3;
-	public static final int SUDOKU_SIDE_LENGTH = SUDOKU_BLOCK_LENGTH*SUDOKU_BLOCK_LENGTH;
-	public static final int SUDOKU_NUMBER_OF_SQUARES = SUDOKU_SIDE_LENGTH*SUDOKU_SIDE_LENGTH;
-	/*Blocks[] numbered as 
-	 * |1|2|3|
-	 * |4|5|6|
-	 * |7|8|9|*/
-	private SudokuBlock[] blocks;
-	
+	public static final int SUDOKU_SIDE_LENGTH = SUDOKU_BLOCK_LENGTH * SUDOKU_BLOCK_LENGTH;
+	public static final int SUDOKU_NUMBER_OF_SQUARES = SUDOKU_SIDE_LENGTH * SUDOKU_SIDE_LENGTH;
+
+	//The squares that populate blocks/rows/collumns
+	private SudokuSquare[] squares;
+
 	//columns[] 1 to 9, left to right
 	private SudokuColumn[] columns;
 	
 	//rows[] 1 to 9, top to bottom
 	private SudokuRow[] rows;
 	
-	private SudokuSquare[] squares;
-	/*
+	/*Blocks[] numbered as 
+	 * |1|2|3|
+	 * |4|5|6|
+	 * |7|8|9|*/
+	private SudokuBlock[] blocks;
+	
+	/**
+	 * Populates a Sudoku.
+	 * @param numbers Takes an int[] of 89 digits. All must be from 1 to 9, or 0 for empty squares.
 	 * |000102|030405|060708|
 	 * |091011|121314|151617|
 	 * |181920|212223|242526|
@@ -38,23 +46,16 @@ public class Sudoku {
 			throw new SudokuException("Too many numbers given.");
 		}
 		
-		squares = new SudokuSquare[SUDOKU_NUMBER_OF_SQUARES];
 		
-		//initialize squares with input
-		for(int i=0; i < squares.length; i++){
-			squares[i] = new SudokuSquare(numbers[i]);
-		}
+		populateSquares(numbers);
+		populateRows();
+		populateColumns();
+		populateBlocks();
 		
-		SudokuSquare[] squareTemp = new SudokuSquare[SUDOKU_SIDE_LENGTH];
+//		SudokuSquare[] squareTemp = new SudokuSquare[SUDOKU_SIDE_LENGTH];
 		
 		//initialize blocks
-		for(int i=0; i < SUDOKU_SIDE_LENGTH; i++){
-			for(int j=0; j < SUDOKU_SIDE_LENGTH; j++){
-				//i*3 
-				//squareTemp[j] = squares[i*SUDOKU_BLOCK_LENGTH];
-			}
-			blocks[i] = new SudokuBlock({squares[i*9])
-		}
+		
 		
 		//initialize columns
 		
@@ -64,6 +65,36 @@ public class Sudoku {
 		
 	}
 		
+	private void populateSquares(int[] numbers){
+		
+		squares = new SudokuSquare[SUDOKU_NUMBER_OF_SQUARES];
+		
+		for(int i=0; i < squares.length; i++){
+			squares[i] = new SudokuSquare(numbers[i]);
+		}
+	}
+
+	private void populateRows(){
+		
+	}
+	
+	private void populateColumns(){
+		
+	}
+
+	private void populateBlocks(){
+//		for(int i=0; i < SUDOKU_SIDE_LENGTH; i++){
+//			for(int j=0; j < SUDOKU_SIDE_LENGTH; j++){
+//				squareTemp[j] = squares[i*SUDOKU_BLOCK_LENGTH];
+//			}
+//			blocks[i] = new SudokuBlock(squareTemp);
+//		}
+	}
+	
+	
+	/**
+	 * Prints out the whole Sudoku as a string. 
+	 */
 	public String toString(){
 		return "| | | |/n| | | |/n| | | |";
 	}
