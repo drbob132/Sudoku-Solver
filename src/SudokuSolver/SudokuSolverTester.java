a97892c2e11212e40c8f133cc0f8b30fb9dc99af
DrBob <DrBob@10.45.178.92>
2018-04-22 22:52:36 -0700

    Designed an argument interface in GNU style
    All unimplemented, but the idea is there.
    
    Also depreciated an old unused io function.

--- src/SudokuSolver/SudokuSolverTester.java
+++ src/SudokuSolver/SudokuSolverTester.java
@@ -1,9 +1,25 @@
 /**
- * Tester for SudokuSolver
+ * Tester for SudokuSolver - A symmetrical Sudoku solver - 9x9, or (n^2)x(n^2)
  * Operates on taking argument input... file or direct arguments.
  * @author drbob132
- * @version 1.2
- * @date 04/18/2018
+ * @version 1.3
+ * @date 04/22/2018
+ * 
+ * Example form: *todo*
+ * 
+ * Arguments:
+ * *todo*--scale,--size: Defaults to 9x9, also known as scale 3, as in (3^2)x(3^2) = 9x9. Use when specifying the dimensions of the Sudoku when it is not 9x9. 
+ * 							Example: 2 for a 4x4 sudoku, 4 for a 16x16 Sudoku, n for a (n^2)x(n^2)
+ * 							Note: Larger Sudokus are supported, but may be quite difficult to solve. Their complexity scales up exponentially, and is currently untested above scale 4
+ * *todo*--brief: Reduces the output to only the puzzle solution.
+ * *todo*--quiet,--silent: Makes the solver cease any output to console. Best used when specifying an output file.
+ * *todo*--debug: Enables debug messages.
+ * *todo*--help: Prints out possible arguments.
+ * *todo*--verify: Checks the given puzzle for conflicts, then attempts to solve the puzzle, then makes a final report.
+ * *todo*--verbose: Prints out more stats than you might normally need.
+ * *todo*--input-encoding: Used to specify the character format for the input puzzle. When not specified, the solver will use the default for the size of sudoku. *todo*(specified below)
+ * *todo*--print-encoding: Used when you want the puzzle printed in a format other than the input encoding.
+ * *todo* [last argument] is the puzzle in numbers or the path to a file which contains the puzzle, with the same format.
  */
 
 package SudokuSolver;
@@ -16,6 +32,7 @@ import java.util.Scanner;
 
 public class SudokuSolverTester {
 	public static void main(String[] args){
+		final boolean DEBUG = true;
 		String puzzle="";
 		long startTime = 0;
 		long endTime = 0;
@@ -24,6 +41,12 @@ public class SudokuSolverTester {
 		final long nanoToMili = 1000000;
 		
 		//check args
+		if(DEBUG) {
+			for(int i=0; i<args.length; i++) {
+				System.out.println("args[" + i + "] = " + args[i]);
+			}
+		}
+		
 		if(args.length < 1){
 			System.out.println("Please give me something in the arguments!");
 		}else{
@@ -88,11 +111,5 @@ public class SudokuSolverTester {
 			System.out.println("(Time Elapsed: " + miliseconds + "." + decimalPlace + " ms)");
 			
 		}
-	}
-	
-	//totally ripped from StackExchange, then modified.
-	private static String readFile(Path path, Charset encoding) throws IOException {
-		byte[] encoded = Files.readAllBytes(path);
-		return new String(encoded, encoding);
 	}
 }
